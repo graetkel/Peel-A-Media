fac1a903ad60f86a78bd070127fe65f2443ceb04
casey anderson <cga3798@uw.edu>
2018-02-27 17:20:58 -0800

    started cuss list

--- getFreq.py
+++ getFreq.py
@@ -1,4 +1,5 @@
 import wsd_code as wc
+import nltk
 
 var TEXT;
 
@@ -6,3 +7,6 @@ words = file(TEXT, “r”).read().split()
 unqWords = sorted(set(word))
 for word in unqWords:
 	print words.count(word), word 
+
+for w in words:
+	
