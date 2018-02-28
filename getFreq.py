58c690f75e401f48b84d8d72a48e89ac891653d4
casey anderson <cga3798@uw.edu>
2018-02-27 18:02:58 -0800

    added parsing

--- getFreq.py
+++ getFreq.py
@@ -2,12 +2,15 @@
 import nltk
 from nltk.wsd import lesk
 from nltk import corpus
-
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import TreebankWordTokenizer
 inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy'}
 kidFriendlyCount = 0;
 var TEXT;
 
 words = file(TEXT, “r”).read().split()
+sent_tokenize(words)
+tokenWords = TreebankWordTokenizer().tokenize(words)
 unqWords = sorted(set(word))
 for word in unqWords:
 	print words.count(word), word 
