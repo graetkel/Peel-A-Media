9a89f9f220a1ed19442684711b82471928cd6f72
Keldon Fischer <keldonf@D-10-16-5-141.dhcp4.washington.edu>
2018-02-27 18:18:34 -0800

    Added a transcript uploader and added words
    The words I added were from the readme and I add it to the getFreq.py

--- getFreq.py
+++ getFreq.py
@@ -2,14 +2,15 @@
 import nltk
 from nltk.wsd import lesk
 
-inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy',  }
+inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy', 'cunt', 'fucking', 'fucked', 'crap', 'goddamn', 'holy shit', 'motherfucker', 'whore', 'twat', 'Shitbag', 'twat', 'Dickhead', 'Cumwipe', 'Piss', 'pissoff', 'bastard', 'Bitch', 'hell', 'cock'}
 kidFriendlyCount = 0;
 var TEXT;
+
+
 
 words = file(TEXT, “r”).read().split()
 unqWords = sorted(set(word))
 for word in unqWords:
-	print words.count(word), word 
+	print words.count(word), word
 
 for w in words:
-	
