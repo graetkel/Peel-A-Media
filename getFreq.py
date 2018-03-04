bfd755dbbc68dad0fcd147ba2683cdba704d3723
casey anderson <cga3798@uw.edu>
2018-03-03 20:57:41 -0800

    seperated syntax parsing from freq class

--- getFreq.py
+++ getFreq.py
@@ -1,16 +1,7 @@
 
 import nltk
-from nltk.wsd import lesk
-
-from nltk import corpus
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.tokenize import TreebankWordTokenizer
-inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy'}
-kidFriendlyCount = 0
 TEXT = open('../Peel-A-Media/Buzzfeed.txt', 'r')
 words = TEXT.read().split()
-#sent_tokenize(words)
-#tokenWords = TreebankWordTokenizer().tokenize(words)
 unqWords = sorted(set(words))
 for word in unqWords:
 	print(words.count(word), word) 
---
+++ getSynParse.py
@@ -0,0 +1,15 @@
+import nltk
+from nltk.wsd import lesk
+
+from nltk import corpus
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+
+inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy'}
+kidFriendlyCount = 0
+TEXT = open('../Peel-A-Media/Buzzfeed.txt', 'r')
+words = TEXT.read()
+sent_tokenize(words)
+
+tokenWords = TreebankWordTokenizer().tokenize(words)
+print(tokenWords)
