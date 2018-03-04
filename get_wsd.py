7b273719a44e4ba3259cf8f3d45cf8d2c8372e5a
irenefransiga <irene.fransiga@yahoo.com>
2018-03-04 10:59:04 -0800

    Add files via upload

---
+++ get_wsd.py
@@ -0,0 +1,26 @@
+from nltk.wsd import lesk
+from nltk.corpus import wordnet as wn
+import string
+import sys
+
+
+# Word Sense Disambiguation
+TEXT = "San Holo Transcript.txt"
+
+words = file(TEXT, "r").read().split()
+unqWords = sorted(set(words))
+# removing punctuation
+unqWords = [''.join(c for c in s if c not in string.punctuation)for s in unqWords]
+# removing numbers (time)
+unqWords = [''.join(c for c in s if not c.isdigit())for s in unqWords]
+
+# removing empty string
+unqWords = [s for s in unqWords if s]
+print(unqWords)
+
+#finding the synsets
+for loop in unqWords:
+    sys.stdout.write(loop + ": ")
+    sys.stdout.flush()
+    #still don't know how to get rid of the none
+    print (lesk(unqWords, loop))
