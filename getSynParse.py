0df6d4fc1cf8ea9dcf24f87343c1cda0e2bee324
casey anderson <cga3798@uw.edu>
2018-03-04 12:03:27 -0800

    update to getSynParse breaks words down to stims

--- getSynParse.py
+++ getSynParse.py
@@ -5,19 +5,22 @@ from nltk.tag import pos_tag, map_tag
 from nltk import corpus
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.tokenize import TreebankWordTokenizer
+from nltk.stem import PorterStemmer
+
+ps = PorterStemmer()
 
 inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy'}
 kidFriendlyCount = 0
 TEXT = open('../Peel-A-Media/Buzzfeed.txt', 'r')
 words = TEXT.read()
 sent_tokenize(words)
-
 tokenWords = word_tokenize(words)
 tokenWords = pos_tag(tokenWords)
 for word in tokenWords:
-    print(word[0])
-    if word[0] in inappropriateWordList:
+    if word[0] is not None:
+        temp = ps.stem(word[0])
+    if temp in inappropriateWordList:
         if word[1] != "NNP":
             kidFriendlyCount += 1
-print(tokenWords)
+#print(tokenWords)
 print(kidFriendlyCount)
