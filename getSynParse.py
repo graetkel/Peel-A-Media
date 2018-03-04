041572422f13e81829e3d9c77219758ecec9d79d
casey anderson <cga3798@uw.edu>
2018-03-04 15:51:58 -0800

    added Keldon test file

--- getSynParse.py
+++ getSynParse.py
@@ -12,7 +12,7 @@ ps = PorterStemmer()
 inappropriateWordList = {'shit', 'ass', 'fuck', 'damn', 'dick', 'pussy', 'whore', 'cunt', 'crap'
 , 'goddman', 'cum', 'piss', 'bitch', 'hell', 'cock', 'goddamn'}
 kidFriendlyCount = 0
-TEXT = open('../Peel-A-Media/JohnWickBabaYaga.txt', 'r')
+TEXT = open('../Peel-A-Media/test.txt', 'r')
 words = TEXT.read()
 sent_tokenize(words)
 tokenWords = word_tokenize(words)
