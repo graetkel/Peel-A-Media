bdaff618983736783d7a4b5cdb8c241caa6dfea6
casey anderson <cga3798@uw.edu>
2018-03-11 11:35:35 -0700

    working on get_sub

--- get_sub.py
+++ get_sub.py
@@ -2,5 +2,9 @@ import spacy
 nlp = spacy.load('en')
 def getSub( arg1 ):
     doc=nlp(arg1)
-    sub_toks = [tok for tok in doc if (tok.dep_ == "nsubj") ]
-    return sub_toks
+    for tok in doc: 
+        print(tok.dep_)
+    #return sub_toks
+
+sent = "does he talk about sports"
+getSub(sent)
